2f9d6597ab5a57ae65c3cb07269d61db0cf3fd8b
SomeoneNamedMarc <thatguymarc0504@gmail.com>
2025-06-02 11:52:51 +0200

    Added Node API

---
+++ AHREM_NODE_API/src/models/DeviceData.ts
@@ -0,0 +1,11 @@
+export interface DeviceData {
+  ID: number;
+  RoomName: string;
+  Temperature: number;
+  Humidity: number;
+  Radon: number;
+  PPM: number;
+  AirQuality: number;
+  DeviceID: number;
+  TimeStamp: Date;
+}
---
+++ AHREM_NODE_API/src/models/User.ts
@@ -0,0 +1,6 @@
+export interface User {
+  ID: number;
+  Email: string;
+  Password: string;
+  IsAdmin: boolean;
+}
