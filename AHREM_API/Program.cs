c1d6c3800c04e731fa05ba0a0291f0cdc4fedde4
SomeoneNamedMarc <thatguymarc0504@gmail.com>
2025-06-04 11:04:35 +0200

    Added httpclient to builder

--- AHREM_API/Program.cs
+++ AHREM_API/Program.cs
@@ -46,6 +46,7 @@ namespace AHREM_API
 
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
+            builder.Services.AddHttpClient();
 
             var app = builder.Build();
 
